7349346621077d0cca1876e800709f5bbf1cd26e
Miha Krajnc <miha.krajnc@dewesoft.com>
2023-12-28 16:13:51 +0100

    Add basic Designer component

---
+++ src/lib/Designer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import * as THREE from 'three'
+
+    let scene: THREE.Scene;
+    let camera: THREE.Camera;
+    let renderer: THREE.WebGLRenderer;
+
+    onMount(() => {
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.getElementById('canvas')?.appendChild(renderer.domElement);
+    });
+</script>
+
+<div id='canvas'/>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
 	import '../app.postcss';
+	import {AppShell, AppRail, AppRailTile} from '@skeletonlabs/skeleton'
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<AppRailTile group="" name="tile-1" value={0} title="tile-1">
+				Item 1
+			</AppRailTile>
+			<AppRailTile group="" name="tile-2" value={0} title="tile-2">
+				Item 2
+			</AppRailTile>
+		</AppRail>
+	</svelte:fragment>
+
+	<slot />
+</AppShell>
