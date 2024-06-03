2b0db6a048540619d7c2030250b81e215be8925a
Miha Krajnc <miha.krajnc8@gmail.com>
2024-06-03 21:22:00 +0200

    add app rail functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,8 @@
 	const modalRegistry: Record<string, ModalComponent> = {
 		simModelOptions: { ref: SimModelOptions },
 	};
+
+	let selectedTab: number = 0;
 </script>
 
 <Modal components={modalRegistry}/>
@@ -19,12 +21,12 @@
 <AppShell>
 	<svelte:fragment slot="sidebarLeft">
 		<AppRail width="w-16"> 
-			<AppRailTile group="" name="tile-1" value={0} title="tile-1">
+			<AppRailTile bind:group={selectedTab} name="tile-1" value={0} title="tile-1">
 				<svelte:fragment slot="lead">
 					<Icon width={36} icon="material-symbols:design-services-outline" style="margin: auto;"/>
 				</svelte:fragment>
 			</AppRailTile>
-			<AppRailTile group="" name="tile-2" value={0} title="tile-2">
+			<AppRailTile bind:group={selectedTab} name="tile-2" value={1} title="tile-2">
 				<svelte:fragment slot="lead">
 					<Icon width={36} icon="mdi:chart-bell-curve-cumulative" style="margin: auto;"/>
 				</svelte:fragment>
