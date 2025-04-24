4fe56c2b9655dd620e844ca43d7a8fabc3c88f75
Miha Krajnc <miha.krajnc8@gmail.com>
2025-04-24 21:00:52 +0200

    Add a base visualizer component #16

---
+++ src/lib/analysis/base-data-vis.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { Label } from "$lib/components/ui/label";
+	import type { Snippet } from "svelte";
+ 
+	type Props = {
+		title: string;
+        children: Snippet | undefined;
+	};
+ 
+	let {
+		title,
+        children
+	}: Props = $props();
+</script>
+
+<div class='h-full flex flex-col items-center'>
+    <Label class="text-lg font-semibold">{title}</Label>
+    {@render children?.()}
+</div>
